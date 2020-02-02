08a3880e0e707528389e45308fa29899524029d3
PC <PC@Bones>
2020-02-02 17:19:35 +0100

    Test für Taschenrechner

---
+++ Taschenrechner/src/main/java/com/mycompany/taschenrechner/MainRechner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.taschenrechner;
+
+/**
+ *
+ * @author PC
+ */
+public class MainRechner {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        TaschenrechnerForm form = new TaschenrechnerForm();
+        form.setVisible(true);
+    }
+}
